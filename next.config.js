222b7b831990512ee88d44a49acbcc6423ab6349
croissant <wathyic@gmail.com>
2023-06-03 17:03:52 +0800

    feat:add PWA

--- next.config.js
+++ next.config.js
@@ -4,12 +4,13 @@ const withPWA = require('next-pwa')({
   dest: 'public',
 });
 
-const nextConfig = withPWA({
+const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ['source.unsplash.com', 'images.pexels.com'],
   },
-});
+};
 
-module.exports = nextConfig;
+module.exports =
+  process.env.NODE_ENV === 'development' ? nextConfig : withPWA(nextConfig);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
+import React from 'react';
+import Head from 'next/head';
+import { createTheme, NextUIProvider } from '@nextui-org/react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+
+import Layout from '../components/Layout/Layout';
+
+import '../styles/globals.css';
+
 import type { ReactElement, ReactNode } from 'react';
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
-import { createTheme, NextUIProvider } from '@nextui-org/react';
-import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import Layout from '../components/Layout/Layout';
-import '../styles/globals.css';
 
 export type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -37,22 +42,30 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
 
   return getLayout(
-    <NextThemesProvider
-      defaultTheme="system"
-      storageKey="theme"
-      enableSystem={true}
-      attribute="class"
-      value={{
-        light: lightTheme.className,
-        dark: darkTheme.className,
-      }}
-    >
-      <NextUIProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </NextUIProvider>
-    </NextThemesProvider>
+    <React.Fragment>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        ></meta>
+      </Head>
+      <NextThemesProvider
+        defaultTheme="system"
+        storageKey="theme"
+        enableSystem={true}
+        attribute="class"
+        value={{
+          light: lightTheme.className,
+          dark: darkTheme.className,
+        }}
+      >
+        <NextUIProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </NextUIProvider>
+      </NextThemesProvider>
+    </React.Fragment>
   );
 }
 
